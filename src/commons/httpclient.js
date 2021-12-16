70973abeaa39e4b1779e612c29014238d2269760
Cyapher <carlosgabrielmdiaz@gmail.com>
2021-12-16 15:02:43 +0800

    upd(12/16/21)

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default axios.create({
 
-    baseURL: 'https://reactdiaz.herokuapp.com/api/v1',
+    baseURL: 'aws-spring.herokuapp.com/api/v1',
     headers:{
         'Content-Type':'application/json'
     }
